c163b2633ce55024d28091deda1d62d74deee549
Viktoriya <vfitkovskaya@gmail.com>
2021-03-09 22:30:42 +0200

    added city selection option

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+let city=prompt("Name of the city: ","");
 //show date
 let options = {month: 'long', day: 'numeric', year: 'numeric', weekday: 'short'};
 let today  = new Date();
@@ -7,7 +8,7 @@ let today  = new Date();
 let date=document.querySelector('#date').innerHTML=today.toLocaleDateString("en-US",options);
 
 
-//show sunrise
+
 function convertTime(unixTime, offset){
     let dt = new Date((unixTime + offset) * 1000)
     let h = dt.getHours()
@@ -17,31 +18,28 @@ function convertTime(unixTime, offset){
 }
 
 
-function showSunrise(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&units=metric&appid=99459609326778ff831f3721432b9bd4')
+
+function showWeather(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=99459609326778ff831f3721432b9bd4`)
     .then((response)=>response.json())
     .then((data)=>
-    {   //sunrise
-        let sunrise=document.querySelector('#sunrise');
-        let sRise = convertTime(data.current.sunrise, data.timezone_offset);
-        sunrise.innerHTML=('Sunrise:  '+sRise); 
-        
+    {           
         //icon
-        let iconCode=data.current.weather[0].icon
+        let iconCode=data.weather[0].icon
         let iconUrl="http://openweathermap.org/img/wn/" + iconCode + ".png"
         document.querySelector('#icon').src=iconUrl;
 
         //Locale time
-        document.querySelector('#time').innerHTML=today.toLocaleTimeString('en-US',{timeZone:data.timezone});
+        document.querySelector('#time').innerHTML=today.toLocaleTimeString('en-US',{timeZone:data.sys.timezone});
 
         //humidity
-        document.querySelector('#humidity').innerHTML=('Humidity: '+data.current.humidity+'%')
+        document.querySelector('#humidity').innerHTML=('Humidity: '+data.main.humidity+'%')
 
         //pressure
-        document.querySelector('#pressure').innerHTML=('Pressure: '+data.current.pressure+ ' hPa')
+        document.querySelector('#pressure').innerHTML=('Pressure: '+data.main.pressure+ ' hPa')
 
         //wind
-        document.querySelector('#wind').innerHTML=('Wind: '+data.current.wind_speed	+' km/h'+'   '+degToCompass(data.current.wind_deg))
+        document.querySelector('#wind').innerHTML=('Wind: '+data.wind.speed	+' km/h'+'   '+degToCompass(data.wind.deg))
         function degToCompass(num) {
             var val = Math.floor((num / 22.5) + 0.5);
             var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
@@ -50,14 +48,14 @@ function showSunrise(){
 
         //temp
 
-        document.querySelector('#temp').innerHTML=(data.current.temp+' &#8451');
+        document.querySelector('#temp').innerHTML=(data.main.temp +' &#8451');
 
         //feels like
-        document.querySelector('#feels').innerHTML=("Feels Like: "+data.current.feels_like+' &#8451')
+        document.querySelector('#feels').innerHTML=("Feels Like: "+data.main.feels_like+' &#8451')
 
         //description
-        document.querySelector('#description').innerHTML=data.current.weather[0].description;
+        document.querySelector('#description').innerHTML=data.weather[0].description;
     })
 }
 
-showSunrise();
+showWeather();
